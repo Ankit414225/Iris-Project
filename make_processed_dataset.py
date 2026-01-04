2ca09468390e5caeb3996be520191e7ee27e9bd7
Ankit414225 <ankit784910@gmail.com>
2026-01-04 14:01:10 +0530

    Update make_processed_dataset.py

--- make_processed_dataset.py
+++ make_processed_dataset.py
@@ -1,12 +1,1 @@
-import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.preprocessing import StandardScaler
-iris = load_iris()
-X = iris.data        # features
-y = iris.target      # labels
-feature_names = iris.feature_names
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-processed_df = pd.DataFrame(X_scaled, columns=feature_names)
-processed_df["species"] = y
-processed_df.to_csv("processed_iris_dataset.csv", index=False)
+Remove unused dataset file
